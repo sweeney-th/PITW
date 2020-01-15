dbf39fe73d7020fc3f32b3fb05b57b97fd882435
thadryan <thadryan@leon.router>
2020-01-14 23:57:54 -0500

    doors as dicts

--- Posts/MonteHall/MonteHallLineByLine.py
+++ Posts/MonteHall/MonteHallLineByLine.py
@@ -1,2 +1,21 @@
 
-print("Let's get back in the zone")
+import random
+
+# first we need the potential outcomes
+doors = ["Door1", "Door2", "Door3"]
+
+# then the results we have
+outcomes = ["Win", "Loss", "Loss"]
+
+# randomize the order of wins/losses
+random.shuffle(outcomes)
+
+# associate the wins and losses with a door
+doorsDict = dict(zip(doors, outcomes))
+
+# a function to choose a random door
+def pickRandomDoor(doorsDict):
+    return list(doorsDict.keys())[random.randint(0,2)]
+
+ourDoor = pickRandomDoor(doorsDict)
+
